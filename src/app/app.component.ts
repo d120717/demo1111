8d74609249461f1579d3e06c37eb15b5fb67c5d8
d120717 <sam120717@gmail.com>
2018-11-11 10:57:17 +0800

    使用apisource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,19 @@ import { map } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'callapi';
-  // apiSource = this.http.get('uri');
-  constructor(private http: HttpClient) {
-    http
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .pipe(
-        map((posts: any[]) => {
-          return posts.map((post: any) => {
-            return { id: post.id, title: post.title };
-          });
-        }),
-      )
-      .subscribe(data => {
-        console.log(data);
+  posts = [];
+  apiSource = this.http.get('https://jsonplaceholder.typicode.com/posts').pipe(
+    map((posts: any[]) => {
+      return posts.map((post: any) => {
+        return { id: post.id, title: post.title };
       });
+    }),
+  );
+  constructor(private http: HttpClient) {}
+
+  loaddata() {
+    this.apiSource.subscribe(data => {
+      this.posts = data;
+    });
   }
 }
