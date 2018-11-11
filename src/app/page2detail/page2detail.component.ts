5e358b4547e7744dc4f79b59b950776cb34bbc26
d120717 <sam120717@gmail.com>
2018-11-11 13:13:58 +0800

    no message

--- src/app/page2detail/page2detail.component.ts
+++ src/app/page2detail/page2detail.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-page2detail',
   templateUrl: './page2detail.component.html',
-  styleUrls: ['./page2detail.component.css']
+  styleUrls: ['./page2detail.component.css'],
 })
 export class Page2detailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute) {
+    route.paramMap.subscribe(param => console.log(param.get('id')));
   }
 
+  ngOnInit() {}
 }
