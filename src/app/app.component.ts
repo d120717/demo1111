6000b3fa7a7c186695dbf4d13b2ba82796445c20
d120717 <sam120717@gmail.com>
2018-11-11 10:45:11 +0800

    post api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'callapi';
+  // apiSource = this.http.get('uri');
   constructor(private http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(data => {
-      console.log(data);
-    });
+    http
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .pipe(
+        map((posts: any[]) => {
+          return posts.map((post: any) => {
+            return { id: post.id, title: post.title };
+          });
+        }),
+      )
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 }
