01c80cb6c2f455fe86d7c1567ad24057325e10db
d120717 <sam120717@gmail.com>
2018-11-11 10:27:08 +0800

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'callapi';
+  constructor(private http: HttpClient) {
+    http.get('https://jsonplaceholder.typicode.com/todos/1').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
